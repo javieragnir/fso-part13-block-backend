9556cddbb73a68b3cc897bf9bd2f50302880a3e0
javier <jjr.agnir@gmail.com>
2022-09-22 21:30:21 +0800

    finish 13.8

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ const { PORT } = require('./util/config')
 const { connectToDatabase } = require('./util/db')
 
 const notesRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+
 const { errorHandler } = require('./util/middleware')
 
 app.use(express.json())
 
 app.use('/api/blogs', notesRouter)
+app.use('/api/users', usersRouter)
 
+// put errorhandler last
 app.use(errorHandler)
 
 const start = async () => {
--- util/middleware.js
+++ util/middleware.js
@@ -1,10 +1,9 @@
+//todo: throw more specific error for updating likes?
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
   if (error.name === 'SequelizeValidationError') {
     return response.status(400).send({ error: error.message })
-  } else {
-    return response.status(500).send({ error: `Something happened! Error: ${error.message}`})
   }
 }
 
